1ff631ac128c936c3b32d4efae3b78fdfa9206ed
hannes <hannesdewaal@gmail.com>
2020-05-28 14:12:09 +0200

    Add stock s3 ioutput implementation

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,9 @@ import (
 	"github.com/Jeffail/benthos/v3/lib/service"
 
 	// Add your plugin packages here
-	_ "github.com/windhooked/benthos-plugins/input"
+//	_ "github.com/windhooked/benthos-plugins/input"
 	_ "github.com/windhooked/benthos-plugins/output"
-	_ "github.com/windhooked/benthos-plugins/processor"
+//	_ "github.com/windhooked/benthos-plugins/processor"
 )
 
 func main() {
